e16db077ddf9a5355fdd7f5a8db2e39e5d25c09e
Damian at mba <damian@frey.NOSPAMco.nz>
2022-10-10 19:40:18 +0200

    testing stuff

---
+++ docker-build/start-runpodio.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "pod started"
+
+if [[ $PUBLIC_KEY ]]
+then
+    mkdir -p ~/.ssh
+    chmod 700 ~/.ssh
+    cd ~/.ssh
+    echo $PUBLIC_KEY >> authorized_keys
+    chmod 700 -R ~/.ssh
+    cd /
+    service ssh start
+    echo "ssh server listening for connections"
+else
+    echo "no \$PUBLIC_KEY set, not running ssh server"
+fi
+
+if [[ $JUPYTER_PASSWORD ]]
+then
+    cd /
+    jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace
+else
+    echo "no \$JUPYTER_PASSWORD set, sleeping forever instead"
+    sleep infinity
+fi
